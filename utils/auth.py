d39da554bf899be12bdf6cd1684c6bebddc81a7a
Max Bertfield <mbertfield@stuy.edu>
2017-07-07 14:55:49 -0400

    disable sqlite3 feaures to be able to deploy on heroku

--- utils/auth.py
+++ utils/auth.py
@@ -2,13 +2,12 @@ import hashlib, sqlite3
 db1 = "misc/database.db"
 def checkLogin(username,password):
     hashedPass = hashlib.sha1(password).hexdigest()
-    db = sqlite3.connect(db1)
-    c = db.cursor()
-    query = "SELECT * FROM users"
-    dbUserPass = c.execute(query)
-   # for entry in dbUserPass:
-   #     if (entry[0] == username):
-   #         if (entry[1] == hashedPass): return ""
-   #         else: return "Incorrect Password"
-   # return "Incorrect Username"
-    return ""
+   # db = sqlite3.connect(db1)
+   # c = db.cursor()
+   # query = "SELECT * FROM users"
+    dbUserPass = [['admin', 'd033e22ae348aeb5660fc2140aec35850c4da997']] #c.execute(query)
+    for entry in dbUserPass:
+        if (entry[0] == username):
+            if (entry[1] == hashedPass): return ""
+            else: return "Incorrect Password"
+    return "Incorrect Username"
