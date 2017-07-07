22e4de5356e86c612a3640a56ce8cc75bbc9afae
Max Bertfield <mbertfield@stuy.edu>
2017-07-07 15:13:27 -0400

    minor change

--- utils/auth.py
+++ utils/auth.py
@@ -1,7 +1,7 @@
 import hashlib, sqlite3
 db1 = "misc/database.db"
 def checkLogin(username,password):
-    hashedPass = hashlib.sha1(password).hexdigest()
+   # hashedPass = hashlib.sha1(password).hexdigest()
    # db = sqlite3.connect(db1)
    # c = db.cursor()
    # query = "SELECT * FROM users"
