f940f5c472492751adfd297af79aba37e67a5e51
Max Bertfield <mbertfield@stuy.edu>
2017-07-07 14:39:02 -0400

    bug squashing

--- utils/auth.py
+++ utils/auth.py
@@ -1,13 +1,14 @@
 import hashlib, sqlite3
 db1 = "misc/database.db"
 def checkLogin(username,password):
-    hashedPass = hashlib.sha1(password).hexdigest()
-    db = sqlite3.connect(db1)
-    c = db.cursor()
-    query = "SELECT * FROM users"
-    dbUserPass = c.execute(query)
-    for entry in dbUserPass:
-        if (entry[0] == username):
-            if (entry[1] == hashedPass): return ""
-            else: return "Incorrect Password"
-    return "Incorrect Username"
+   # hashedPass = hashlib.sha1(password).hexdigest()
+   # db = sqlite3.connect(db1)
+   # c = db.cursor()
+   # query = "SELECT * FROM users"
+   # dbUserPass = c.execute(query)
+   # for entry in dbUserPass:
+   #     if (entry[0] == username):
+   #         if (entry[1] == hashedPass): return ""
+   #         else: return "Incorrect Password"
+   # return "Incorrect Username"
+    return ""
