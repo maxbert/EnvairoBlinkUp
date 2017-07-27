b5d80bc8bc8cea7429f39bb0ec7fe93afeaa20a0
Max Bertfield <mbertfield@stuy.edu>
2017-07-27 12:56:55 -0400

    travis stuff

--- deployment/bin/setup_deploy_keys.sh
+++ deployment/bin/setup_deploy_keys.sh
@@ -12,8 +12,8 @@ ssh-keygen -t rsa -b 4096 -C "build@travis-ci.org" -f ./deployment/config/deploy
 travis encrypt-file ./deployment/config/deploy_rsa --add
 mv deploy_rsa.enc ./deployment/config/deploy_rsa.enc
 
-add_key "ec2-user@app.envairo.com"
-add_key "ec2-user@staging.envairo.com"
+add_key "ec2-user@dashboard.envairo.com"
+
 
 echo "Deploy key created. Please save your private key in a safe place, then remove it from the repository."
 echo "E.g., rm deployment/config/deploy_rsa"
